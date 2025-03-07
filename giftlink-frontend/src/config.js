e298f15634966e8fd57483b7a181ed45af6668ff
Kentaro <kenkou.ishi.1111@gmail.com>
2025-03-07 08:22:46 -0500

    comment added

--- giftlink-frontend/src/config.js
+++ giftlink-frontend/src/config.js
@@ -1,6 +1,11 @@
+// 設定オブジェクトを作成
 const config = {
+  // 環境変数からバックエンドのURLを取得して設定
   backendUrl: process.env.REACT_APP_BACKEND_URL,
 };
 
+// コンソールにバックエンドのURLを出力（デバッグ用）
 console.log(`backendUrl in config.js: ${config.backendUrl}`)
+
+// configオブジェクトをurlConfigとしてエクスポート
 export {config as urlConfig}
